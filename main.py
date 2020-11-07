5baaffb6a4995e0d545c36bde7377977c30fd4e2
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-07 17:23:22 +0000

    Uploading project files to GitHub.

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 # 🚨 Don't change the code below 👇
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? (S, M, or L): ")
+add_pepperoni = input("Do you want pepperoni? (Y/N:) ")
+extra_cheese = input("Do you want extra cheese? (Y/N:) ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+bill = 0
+
+if size == "S":
+  bill += 15
+  
+elif size == "M":
+  bill += 20
+elif size == "L":
+  bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+      bill += 2
+    else:
+      bill += 3
+
+if extra_cheese == "Y":
+  bill += 1
+
+
+print(f"Your bill total is: {bill}")
+    
+  
 
 
 
+'''
+Pepperoni for Small Pizza: +$2
+Pepperoni for Medium or Large Pizza: +$3
+Extra cheese for any size pizza: + $1
+'''
 
 
